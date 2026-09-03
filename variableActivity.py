e27bff0a14d8aa5c1231ef1ac4ddfb65126c1a77
angelm394 <amcfarland736@boyslatin.net>
2026-09-03 11:59:56 +0000

    class notes

---
+++ variableActivity.py
@@ -0,0 +1,39 @@
+# VARIABLES - a container that stores data. 
+# you can  name your container almost anything  
+# you want, so long as it is descriptive. 
+
+# Variables operate on the priniciple of Key / Value pairs
+
+user = 'Ian' # a variable storing a string
+yearsOfExperience = 10 # a variable storing a number
+graduated = True # a variable storing a boolean
+
+Ja3SneakersCount = 9
+Ja3SneakersPrice = 150.00
+Ja3SneakersSKU = "91022382913"
+
+Douglas42="" 
+S21average="" 
+
+
+# When we want to create a variable, we make up a word, 
+# use the equal sign and then assign it a data type.  
+# whenever the computer sees our variable word it will
+# alway use the data inside. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
